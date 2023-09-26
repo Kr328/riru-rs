5614661906a09244f78aaf87c2a2b43913eff3d0
Kr328 <kr328app@outlook.com>
2023-09-27 07:25:06 +0800

    Initial: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,292 @@
+use std::{
+    ffi::{CStr, CString},
+    ptr::null_mut,
+};
+
+use jni_sys::{jboolean, jclass, jint, jintArray, jlong, jobjectArray, jstring, JNIEnv};
+use libc::{c_int, gid_t, uid_t};
+
+use crate::sys::{Riru, RiruModuleInfo, RiruVersionedModuleInfo};
+
+mod sys;
+
+pub struct ForkAndSpecializeArgs<'a> {
+    pub uid: Option<&'a mut jint>,
+    pub gid: Option<&'a mut jint>,
+    pub gids: Option<&'a mut jintArray>,
+    pub runtime_flags: Option<&'a mut jint>,
+    pub rlimits: Option<&'a mut jobjectArray>,
+    pub mount_external: Option<&'a mut jint>,
+    pub se_info: Option<&'a mut jstring>,
+    pub nice_name: Option<&'a mut jstring>,
+    pub fds_to_close: Option<&'a mut jintArray>,
+    pub fds_to_ignore: Option<&'a mut jintArray>,
+    pub is_child_zygote: Option<&'a mut jboolean>,
+    pub instruction_set: Option<&'a mut jstring>,
+    pub app_data_dir: Option<&'a mut jstring>,
+    pub is_top_app: Option<&'a mut jboolean>,
+    pub pkg_data_info_list: Option<&'a mut jobjectArray>,
+    pub whitelisted_data_info_list: Option<&'a mut jobjectArray>,
+    pub bind_mount_app_data_dirs: Option<&'a mut jboolean>,
+    pub bind_mount_app_storage_dirs: Option<&'a mut jboolean>,
+}
+
+pub struct ForkSystemServerArgs<'a> {
+    pub uid: Option<&'a mut uid_t>,
+    pub gid: Option<&'a mut gid_t>,
+    pub gids: Option<&'a mut jintArray>,
+    pub runtime_flags: Option<&'a mut jint>,
+    pub rlimits: Option<&'a mut jobjectArray>,
+    pub permitted_capabilities: Option<&'a mut jlong>,
+    pub effective_capabilities: Option<&'a mut jlong>,
+}
+
+pub struct SpecializeAppProcessArgs<'a> {
+    pub uid: Option<&'a mut jint>,
+    pub gid: Option<&'a mut jint>,
+    pub gids: Option<&'a mut jintArray>,
+    pub runtime_flags: Option<&'a mut jint>,
+    pub rlimits: Option<&'a mut jobjectArray>,
+    pub mount_external: Option<&'a mut jint>,
+    pub se_info: Option<&'a mut jstring>,
+    pub nice_name: Option<&'a mut jstring>,
+    pub start_child_zygote: Option<&'a mut jboolean>,
+    pub instruction_set: Option<&'a mut jstring>,
+    pub app_data_dir: Option<&'a mut jstring>,
+    pub is_top_app: Option<&'a mut jboolean>,
+    pub pkg_data_info_list: Option<&'a mut jobjectArray>,
+    pub whitelisted_data_info_list: Option<&'a mut jobjectArray>,
+    pub bind_mount_app_data_dirs: Option<&'a mut jboolean>,
+    pub bind_mount_app_storage_dirs: Option<&'a mut jboolean>,
+}
+
+pub enum ForkResult {
+    OnParent(libc::pid_t),
+    OnChild,
+}
+
+pub trait ModuleTable {
+    fn should_skip_uid(&mut self, uid: jint) -> bool;
+    fn pre_fork_and_specialize(&mut self, env: *mut JNIEnv, args: &mut ForkAndSpecializeArgs);
+    fn post_fork_and_specialize(&mut self, env: *mut JNIEnv, result: ForkResult);
+    fn pre_fork_system_server(&mut self, env: *mut JNIEnv, args: &mut ForkSystemServerArgs);
+    fn post_fork_system_server(&mut self, env: *mut JNIEnv, result: ForkResult);
+    fn pre_specialize_app_process(&mut self, env: *mut JNIEnv, args: &mut SpecializeAppProcessArgs);
+    fn post_specialize_app_process(&mut self, env: *mut JNIEnv);
+}
+
+pub trait Module: ModuleTable {
+    fn support_hide(&self) -> bool;
+    fn version(&self) -> i32;
+    fn version_name(&self) -> String;
+
+    fn new(api: Api) -> Self;
+}
+
+pub struct Api {
+    module_path: String,
+    allow_unload: Option<&'static mut c_int>,
+}
+
+impl Api {
+    pub fn set_allow_unload(&mut self, allow_unload: bool) {
+        if let Some(mark) = &mut self.allow_unload {
+            **mark = if allow_unload { 1 } else { 0 }
+        }
+    }
+
+    pub fn get_module_path(&self) -> &str {
+        &self.module_path
+    }
+}
+
+#[macro_export]
+macro_rules! register_riru_module {
+    ($module:ty) => {
+        #[no_mangle]
+        pub unsafe extern "C" fn init(riru: *const ::std::ffi::c_void) -> *mut ::std::ffi::c_void {
+            $crate::module_entry::<$module>(riru.cast()).cast()
+        }
+    };
+}
+
+static mut MODULE_TABLE: Option<Box<dyn ModuleTable>> = None;
+
+unsafe extern "C" fn module_should_skip_uid(uid: c_int) -> c_int {
+    if MODULE_TABLE.as_mut().unwrap().should_skip_uid(uid) {
+        1
+    } else {
+        0
+    }
+}
+
+unsafe extern "C" fn module_fork_and_specialize_pre(
+    env: *mut JNIEnv,
+    _: jclass,
+    uid: *mut jint,
+    gid: *mut jint,
+    gids: *mut jintArray,
+    runtime_flags: *mut jint,
+    rlimits: *mut jobjectArray,
+    mount_external: *mut jint,
+    se_info: *mut jstring,
+    nice_name: *mut jstring,
+    fds_to_close: *mut jintArray,
+    fds_to_ignore: *mut jintArray,
+    is_child_zygote: *mut jboolean,
+    instruction_set: *mut jstring,
+    app_data_dir: *mut jstring,
+    is_top_app: *mut jboolean,
+    pkg_data_info_list: *mut jobjectArray,
+    whitelisted_data_info_list: *mut jobjectArray,
+    bind_mount_app_data_dirs: *mut jboolean,
+    bind_mount_app_storage_dirs: *mut jboolean,
+) {
+    let mut args = ForkAndSpecializeArgs {
+        uid: uid.as_mut(),
+        gid: gid.as_mut(),
+        gids: gids.as_mut(),
+        runtime_flags: runtime_flags.as_mut(),
+        rlimits: rlimits.as_mut(),
+        mount_external: mount_external.as_mut(),
+        se_info: se_info.as_mut(),
+        nice_name: nice_name.as_mut(),
+        fds_to_close: fds_to_close.as_mut(),
+        fds_to_ignore: fds_to_ignore.as_mut(),
+        is_child_zygote: is_child_zygote.as_mut(),
+        instruction_set: instruction_set.as_mut(),
+        app_data_dir: app_data_dir.as_mut(),
+        is_top_app: is_top_app.as_mut(),
+        pkg_data_info_list: pkg_data_info_list.as_mut(),
+        whitelisted_data_info_list: whitelisted_data_info_list.as_mut(),
+        bind_mount_app_data_dirs: bind_mount_app_data_dirs.as_mut(),
+        bind_mount_app_storage_dirs: bind_mount_app_storage_dirs.as_mut(),
+    };
+
+    MODULE_TABLE.as_mut().unwrap().pre_fork_and_specialize(env, &mut args);
+}
+
+unsafe extern "C" fn module_fork_and_specialize_post(env: *mut JNIEnv, _: jclass, res: jint) {
+    let result = if res == 0 {
+        ForkResult::OnChild
+    } else {
+        ForkResult::OnParent(res as libc::pid_t)
+    };
+
+    MODULE_TABLE.as_mut().unwrap().post_fork_and_specialize(env, result);
+}
+
+unsafe extern "C" fn module_fork_system_server_pre(
+    env: *mut JNIEnv,
+    _: jclass,
+    uid: *mut uid_t,
+    gid: *mut gid_t,
+    gids: *mut jintArray,
+    runtime_flags: *mut jint,
+    rlimits: *mut jobjectArray,
+    permitted_capabilities: *mut jlong,
+    effective_capabilities: *mut jlong,
+) {
+    let mut args = ForkSystemServerArgs {
+        uid: uid.as_mut(),
+        gid: gid.as_mut(),
+        gids: gids.as_mut(),
+        runtime_flags: runtime_flags.as_mut(),
+        rlimits: rlimits.as_mut(),
+        permitted_capabilities: permitted_capabilities.as_mut(),
+        effective_capabilities: effective_capabilities.as_mut(),
+    };
+
+    MODULE_TABLE.as_mut().unwrap().pre_fork_system_server(env, &mut args);
+}
+
+unsafe extern "C" fn module_fork_system_server_post(env: *mut JNIEnv, _: jclass, res: jint) {
+    let result = if res == 0 {
+        ForkResult::OnChild
+    } else {
+        ForkResult::OnParent(res as libc::pid_t)
+    };
+
+    MODULE_TABLE.as_mut().unwrap().post_fork_system_server(env, result);
+}
+
+unsafe extern "C" fn module_specialize_app_process_pre(
+    env: *mut JNIEnv,
+    _: jclass,
+    uid: *mut jint,
+    gid: *mut jint,
+    gids: *mut jintArray,
+    runtime_flags: *mut jint,
+    rlimits: *mut jobjectArray,
+    mount_external: *mut jint,
+    se_info: *mut jstring,
+    nice_name: *mut jstring,
+    start_child_zygote: *mut jboolean,
+    instruction_set: *mut jstring,
+    app_data_dir: *mut jstring,
+    is_top_app: *mut jboolean,
+    pkg_data_info_list: *mut jobjectArray,
+    whitelisted_data_info_list: *mut jobjectArray,
+    bind_mount_app_data_dirs: *mut jboolean,
+    bind_mount_app_storage_dirs: *mut jboolean,
+) {
+    let mut args = SpecializeAppProcessArgs {
+        uid: uid.as_mut(),
+        gid: gid.as_mut(),
+        gids: gids.as_mut(),
+        runtime_flags: runtime_flags.as_mut(),
+        rlimits: rlimits.as_mut(),
+        mount_external: mount_external.as_mut(),
+        se_info: se_info.as_mut(),
+        nice_name: nice_name.as_mut(),
+        start_child_zygote: start_child_zygote.as_mut(),
+        instruction_set: instruction_set.as_mut(),
+        app_data_dir: app_data_dir.as_mut(),
+        is_top_app: is_top_app.as_mut(),
+        pkg_data_info_list: pkg_data_info_list.as_mut(),
+        whitelisted_data_info_list: whitelisted_data_info_list.as_mut(),
+        bind_mount_app_data_dirs: bind_mount_app_data_dirs.as_mut(),
+        bind_mount_app_storage_dirs: bind_mount_app_storage_dirs.as_mut(),
+    };
+
+    MODULE_TABLE.as_mut().unwrap().pre_specialize_app_process(env, &mut args);
+}
+
+unsafe extern "C" fn module_specialize_app_process_post(env: *mut JNIEnv, _: jclass) {
+    MODULE_TABLE.as_mut().unwrap().post_specialize_app_process(env);
+}
+
+#[doc(hidden)]
+pub unsafe fn module_entry<T: Module + 'static>(riru: *const Riru) -> *mut RiruVersionedModuleInfo {
+    if (*riru).riru_api_version < sys::API_VERSION {
+        return null_mut();
+    }
+
+    let api = Api {
+        module_path: CStr::from_ptr((*riru).magisk_module_path).to_str().unwrap().to_string(),
+        allow_unload: (*riru).allow_unload.as_mut(),
+    };
+
+    let module = T::new(api);
+
+    let info = RiruVersionedModuleInfo {
+        module_api_version: sys::API_VERSION,
+        module_info: RiruModuleInfo {
+            support_hide: if module.support_hide() { 1 } else { 0 },
+            version: module.version(),
+            version_name: CString::new(module.version_name()).unwrap().into_raw(),
+            on_module_loaded: None,
+            should_skip_uid: Some(module_should_skip_uid),
+            fork_and_specialize_pre: Some(module_fork_and_specialize_pre),
+            fork_and_specialize_post: Some(module_fork_and_specialize_post),
+            fork_system_server_pre: Some(module_fork_system_server_pre),
+            fork_system_server_post: Some(module_fork_system_server_post),
+            specialize_app_process_pre: Some(module_specialize_app_process_pre),
+            specialize_app_process_post: Some(module_specialize_app_process_post),
+        },
+    };
+
+    MODULE_TABLE = Some(Box::new(module));
+
+    Box::into_raw(Box::new(info))
+}
---
+++ src/sys.rs
@@ -0,0 +1,103 @@
+use std::ffi::{c_char, c_int, c_void};
+
+use jni_sys::{jboolean, jclass, jint, jintArray, jlong, jobjectArray, jstring, JNIEnv};
+use libc::{gid_t, uid_t};
+
+pub const API_VERSION: c_int = 26;
+
+pub type OnModuleLoadedFunc = Option<unsafe extern "C" fn()>;
+pub type ShouldSkipUidFunc = Option<unsafe extern "C" fn(uid: c_int) -> c_int>;
+pub type NativeForkAndSpecializePreFunc = Option<
+    unsafe extern "C" fn(
+        env: *mut JNIEnv,
+        cls: jclass,
+        uid: *mut jint,
+        gid: *mut jint,
+        gids: *mut jintArray,
+        runtime_flags: *mut jint,
+        rlimits: *mut jobjectArray,
+        mount_external: *mut jint,
+        se_info: *mut jstring,
+        nice_name: *mut jstring,
+        fds_to_close: *mut jintArray,
+        fds_to_ignore: *mut jintArray,
+        is_child_zygote: *mut jboolean,
+        instruction_set: *mut jstring,
+        app_data_dir: *mut jstring,
+        is_top_app: *mut jboolean,
+        pkg_data_info_list: *mut jobjectArray,
+        whitelisted_data_info_list: *mut jobjectArray,
+        bind_mount_app_data_dirs: *mut jboolean,
+        bind_mount_app_storage_dirs: *mut jboolean,
+    ),
+>;
+pub type NativeForkAndSpecializePostFunc = Option<unsafe extern "C" fn(env: *mut JNIEnv, cls: jclass, res: jint)>;
+pub type NativeForkSystemServerPreFunc = Option<
+    unsafe extern "C" fn(
+        env: *mut JNIEnv,
+        cls: jclass,
+        uid: *mut uid_t,
+        gid: *mut gid_t,
+        gids: *mut jintArray,
+        runtime_flags: *mut jint,
+        rlimits: *mut jobjectArray,
+        permitted_capabilities: *mut jlong,
+        effective_capabilities: *mut jlong,
+    ),
+>;
+pub type NativeForkSystemServerPostFunc = Option<unsafe extern "C" fn(env: *mut JNIEnv, cls: jclass, res: jint)>;
+pub type NativeSpecializeAppProcessPreFunc = Option<
+    unsafe extern "C" fn(
+        env: *mut JNIEnv,
+        cls: jclass,
+        uid: *mut jint,
+        gid: *mut jint,
+        gids: *mut jintArray,
+        runtime_flags: *mut jint,
+        rlimits: *mut jobjectArray,
+        mount_external: *mut jint,
+        se_info: *mut jstring,
+        nice_name: *mut jstring,
+        start_child_zygote: *mut jboolean,
+        instruction_set: *mut jstring,
+        app_data_dir: *mut jstring,
+        is_top_app: *mut jboolean,
+        pkg_data_info_list: *mut jobjectArray,
+        whitelisted_data_info_list: *mut jobjectArray,
+        bind_mount_app_data_dirs: *mut jboolean,
+        bind_mount_app_storage_dirs: *mut jboolean,
+    ),
+>;
+pub type NativeSpecializeAppProcessPostFunc = Option<unsafe extern "C" fn(env: *mut JNIEnv, cls: jclass)>;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct RiruModuleInfo {
+    pub support_hide: c_int,
+    pub version: c_int,
+    pub version_name: *const c_char,
+    pub on_module_loaded: OnModuleLoadedFunc,
+    pub should_skip_uid: ShouldSkipUidFunc,
+    pub fork_and_specialize_pre: NativeForkAndSpecializePreFunc,
+    pub fork_and_specialize_post: NativeForkAndSpecializePostFunc,
+    pub fork_system_server_pre: NativeForkSystemServerPreFunc,
+    pub fork_system_server_post: NativeForkSystemServerPostFunc,
+    pub specialize_app_process_pre: NativeSpecializeAppProcessPreFunc,
+    pub specialize_app_process_post: NativeSpecializeAppProcessPostFunc,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct RiruVersionedModuleInfo {
+    pub module_api_version: c_int,
+    pub module_info: RiruModuleInfo,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct Riru {
+    pub riru_api_version: c_int,
+    pub unused: *mut c_void,
+    pub magisk_module_path: *const c_char,
+    pub allow_unload: *mut c_int,
+}
